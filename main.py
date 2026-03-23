ece15f171886d9b1958328f446adde9e252fffe1
Piotr Bialaszczyk <piotr.bialaszczyk@gmail.com>
2026-03-23 22:23:36 +0100

    remove cors middleware after integrating frontend with backend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from app.db.base import Base
@@ -10,16 +9,6 @@ from app import api
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 app.include_router(api.router)
 
