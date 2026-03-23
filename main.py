afa60ee2abb7b21ed25401a273c1645987788454
Piotr Bialaszczyk <piotr.bialaszczyk@gmail.com>
2026-03-23 21:01:10 +0100

    add fastapi app and database dependency

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from app.db.session import SessionLocal
 from app.db.base import Base
 from app.db.session import engine
 from app.db import models
 
+
 Base.metadata.create_all(bind=engine)
 
-print("Tables created")
+app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def root():
+    return {"message": "Sports Calendar API"}
